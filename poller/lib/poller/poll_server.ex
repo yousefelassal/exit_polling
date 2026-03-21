a8ec7665107f802ee706a925b4363e230299943f
Yousef Elassal <yousef.elassal@shezlong.com>
2026-03-21 16:58:12 +0200

    Add PollServer GenServer for per-district polls
    Register each district process under atom names "district:<id>".
    Expose start_link/1 and synchronous API functions add_question/2,
    vote/2 and get_poll/1. Delegate state management to the Poll module.

---
+++ poller/lib/poller/poll_server.ex
@@ -0,0 +1,50 @@
+defmodule Poller.PollServer do
+  use GenServer
+
+  alias Poller.Poll
+
+  def start_link(district_id) do
+    name = district_name(district_id)
+    GenServer.start_link(__MODULE__, district_id, name: name)
+  end
+
+  defp district_name(district_id) do
+    String.to_atom("district:#{district_id}")
+  end
+
+  def add_question(district_id, question) do
+    name = district_name(district_id)
+    GenServer.call(name, {:add_question, question})
+  end
+
+  def vote(district_id, choice_id) do
+    name = district_name(district_id)
+    GenServer.call(name, {:vote, choice_id})
+  end
+
+  def get_poll(district_id) do
+    name = district_name(district_id)
+    GenServer.call(name, :get_poll)
+  end
+
+  # GenServer callbacks
+
+  def init(district_id) do
+    poll = Poll.new(district_id)
+    {:ok, poll}
+  end
+
+  def handle_call({:add_question, question}, _from, poll) do
+    poll = Poll.add_question(poll, question)
+    {:reply, :ok, poll}
+  end
+
+  def handle_call({:vote, choice_id}, _from, poll) do
+    poll = Poll.vote(poll, choice_id)
+    {:reply, :ok, poll}
+  end
+
+  def handle_call(:get_poll, _from, poll) do
+    {:reply, poll, poll}
+  end
+end
