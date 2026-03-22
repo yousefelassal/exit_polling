3bf9eec2b5ec26283bb0f607cda92a893f00d214
Yousef Elassal <yousef.elassal@shezlong.com>
2026-03-22 17:31:08 +0200

    Add choice query helpers and change_choice

--- poller_dal/lib/poller_dal/choices.ex
+++ poller_dal/lib/poller_dal/choices.ex
@@ -7,6 +7,19 @@ defmodule PollerDal.Choices do
     from(c in Choice, where: c.question_id == ^question_id)
     |> Repo.all()
   end
+  
+  def list_choices_by_district_id(district_id) do
+    from(c in Choice,
+      join: q in assoc(c, :question),
+      where: q.district_id == ^district_id
+    )
+    |> Repo.all()
+  end
+  
+  def list_choices_by_choice_ids(choice_ids) do
+    from(c in Choice, where: c.id in ^choice_ids)
+    |> Repo.all()
+  end
 
   def list_choices, do: Repo.all(Choice)
 
@@ -26,5 +39,9 @@ defmodule PollerDal.Choices do
 
   def delete_choice(%Choice{} = choice) do
     Repo.delete(choice)
+  end
+  
+  def change_choice(%Choice{} = choice) do
+    Choice.changeset(choice, %{})
   end
 end
