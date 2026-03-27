36f5c5b743caa1227e3b6f0e126eb8e6f87ef676
Yousef Elassal <yousef.elassal@shezlong.com>
2026-03-27 15:10:58 +0200

    Render district list on home page
    Fetch districts in the controller, log them, and render a table in the
    template

--- poller_phx/lib/poller_phx_web/controllers/district_controller.ex
+++ poller_phx/lib/poller_phx_web/controllers/district_controller.ex
@@ -1,7 +1,12 @@
 defmodule PollerPhxWeb.DistrictController do
   use PollerPhxWeb, :controller
+  require Logger
+  
+  alias PollerDal.Districts
 
   def home(conn, _params) do
-    render(conn, :home)
+    districts = Districts.list_districts()
+    Logger.info("Districts: #{inspect(districts)}")
+    render(conn, :home, districts: districts)
   end
 end
