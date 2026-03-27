a9dbfa83ead91e30e75b5d24f401080101751d78
Yousef Elassal <yousef.elassal@shezlong.com>
2026-03-27 15:10:42 +0200

    Add poller_dal path dependency

--- poller_phx/mix.exs
+++ poller_phx/mix.exs
@@ -63,7 +63,8 @@ defmodule PollerPhx.MixProject do
       {:gettext, "~> 1.0"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:poller_dal, [path: "../poller_dal"]}
     ]
   end
 
